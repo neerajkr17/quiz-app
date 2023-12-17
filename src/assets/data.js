ac9c57b348bb34d4ff066431a6495f4b8d7355e3
neerajkr17 <neeraj192837@gmail.com>
2023-12-17 23:23:15 +0530

    initial

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+      question: "Identify the command which is used to remove files.",
+      option1: "rd",
+      option2: "remove",
+      option3: "rmdir",
+      option4: "rdir",
+      ans: 3,
+    },
+    {
+      question: "Which continent has the highest number of countries?",
+      option1: "Asia",
+      option2: "Europe",
+      option3: "North America",
+      option4: "Africa",
+      ans: 4,
+    },
+    {
+      question: "Junk e-mail is also called?",
+      option1: "Spam",
+      option2: "Fake",
+      option3: "Archived",
+      option4: "Bin",
+      ans: 1,
+    },
+    {
+      question: "A computer cannot BOOT if it does not have the?",
+      option1: "Application Software",
+      option2: "Internet",
+      option3: "Operating System",
+      option4: "Mouse",
+      ans: 3,
+    },
+    {
+      question: "First page of Website is termed as?",
+      option1: "Index Page",
+      option2: "Homepage",
+      option3: "Sitemap",
+      option4: "Pen Drive",
+      ans: 2,
+    },
+  ];
